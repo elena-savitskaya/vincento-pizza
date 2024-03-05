785917d8fe66e8b325f063b77d6c3f6aa6ac85ef
ElenaS35 <helena0518savitskaya@gmail.com>
2024-03-05 18:03:25 +0200

    change router

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route
           path="cart"
           element={
